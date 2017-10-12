a04306545f8fb8cfe0881533708648a51392a72f
chuckles234 <32748751+chuckles234@users.noreply.github.com>
2017-10-12 15:28:50 -0400

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ app.get('/luca', function (req, res) {
   res.send('Is better than Charlie 👍');
 });
 
+// reply to request with "Hello World!"
+app.get('/chuckles234', function (req, res) {
+  res.send('According to Brock the Rob Johnson, my name is anothy.');
+});
+
 //start a server on port 80 and log its start to our console
 var server = app.listen(80, function () {
 
