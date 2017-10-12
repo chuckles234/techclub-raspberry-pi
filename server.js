53016dd99645ffb9a6ba10f170f6e43b97aa560b
Mattjarowicz124 <32748733+Mattjarowicz124@users.noreply.github.com>
2017-10-12 15:27:40 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,14 @@ app.get('/', function (req, res) {
 
 // reply to request with "Hello World!"
 app.get('/charlie', function (req, res) {
-  res.send('He is a special child. -_-');
+  res.send('He is a special child. 👺');
 });
+
+// reply to request with "Hello World!"
+app.get('/Mattjarowicz124', function (req, res) {
+  res.send('<img src="https://1.cdn.edl.io/PFEZa720lyk061N3jV0Aoxi3Riwnf1xDQb2gitXOXMO7n9n8.jpg"/>');
+});
+
 
 //start a server on port 80 and log its start to our console
 var server = app.listen(80, function () {
