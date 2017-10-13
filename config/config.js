feb0f56b5a68bd4c0a2b8079f6fc9db41592a86d
Luca Santarella <luca.santarella@gmail.com>
2017-10-12 20:50:09 -0400

    Updated config for ENV

--- config/config.js
+++ config/config.js
@@ -1,33 +1,36 @@
 var path = require('path'),
     rootPath = path.normalize(__dirname + '/..'),
-    env = process.env.NODE_ENV || 'development';
+    env = process.env.NODE_ENV || 'development',
+    dbHost = process.env.DB_HOST || 'localhost',
+    dbPort = process.env.DB_PORT || '27018',
+    dbName = process.env.DB_NAME,
+    dbUser = process.env.DB_USER,
+    dbPass = process.env.DB_PASS;
 
 var config = {
   development: {
     root: rootPath,
     app: {
-      name: 'expressyo'
+      name: 'techclub'
     },
     port: process.env.PORT || 3000,
-    db: 'mongodb://localhost/expressyo-development'
+    db: 'mongodb://' + dbUser + ':' + dbPass + '@' + dbHost + ':' + dbPort + '/' + dbName
   },
-
   test: {
     root: rootPath,
     app: {
-      name: 'expressyo'
+      name: 'techclub'
     },
     port: process.env.PORT || 3000,
-    db: 'mongodb://localhost/expressyo-test'
+    db: 'mongodb://' + dbUser + ':' + dbPass + '@' + dbHost + ':' + dbPort + '/' + dbName
   },
-
   production: {
     root: rootPath,
     app: {
-      name: 'expressyo'
+      name: 'techclub'
     },
-    port: process.env.PORT || 3000,
-    db: 'mongodb://localhost/expressyo-production'
+    port: process.env.PORT || 80,
+    db: 'mongodb://' + dbUser + ':' + dbPass + '@' + dbHost + ':' + dbPort + '/' + dbName
   }
 };
 
