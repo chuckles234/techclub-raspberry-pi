4a957eb2354d21da7dd9671b286a3ff7c10355ed
Luca Santarella <luca.santarella@gmail.com>
2017-10-13 10:35:02 -0400

    Querying articles

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -10,7 +10,8 @@ module.exports = function (app) {
 router.get('/', function (req, res, next) {
   Article.find(function (err, articles) {
     if (err) return next(err);
-    res.render('index', {
+      console.log(articles);
+      res.render('index', {
       title: 'Generator-Express MVC',
       articles: articles
     });
