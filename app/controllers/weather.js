8d73160eb85e197494dd3cc6b7fb3cec3bdb18fe
Luca Santarella <luca.santarella@gmail.com>
2017-10-19 12:17:43 -0400

    Cleanup unused imports

--- app/controllers/weather.js
+++ app/controllers/weather.js
@@ -1,8 +1,6 @@
-var express = require('express'),
+const express = require('express'),
   request = require('request'),
-  router = express.Router(),
-  mongoose = require('mongoose'),
-  Article = mongoose.model('Article');
+  router = express.Router();
 
 const WeatherBaseUrl = 'http://api.openweathermap.org/data/2.5/';
 const API_KEY = '602d10eaa16056b9fe71e9d6693e44db';
