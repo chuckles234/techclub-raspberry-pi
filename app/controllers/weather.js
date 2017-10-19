e47bf60d29d51720fbddde00e055f25bdddf7ac1
Luca Santarella <luca.santarella@gmail.com>
2017-10-19 12:17:16 -0400

    Base controller function

--- app/controllers/weather.js
+++ app/controllers/weather.js
@@ -1,54 +1,16 @@
 var express = require('express'),
+  request = require('request'),
   router = express.Router(),
   mongoose = require('mongoose'),
   Article = mongoose.model('Article');
 
+const WeatherBaseUrl = 'http://api.openweathermap.org/data/2.5/';
+const API_KEY = '602d10eaa16056b9fe71e9d6693e44db';
+
 module.exports = function (app) {
-  app.use('/', router);
+  app.use('/weather', router);
 };
 
 router.get('/', function (req, res, next) {
-  Article.find(function (err, articles) {
-    if (err) return next(err);
-    res.render('index', {
-      title: 'Generator-Express MVC',
-      articles: articles
-    });
-  });
-});
 
-router.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-router.get('/charlie', function (req, res) {
-  res.send('He is a special child. 👺');
-});
-
-router.get('/luca', function (req, res) {
-  res.send('The stupidest person in the world with no friends or family. Seriosly, its so bad that its become sad. Pretty funny though.');
-});
-
-router.get('/chuckles234', function (req, res) {
-  res.send('According to Brock the Rob Johnson, my name is anothy.');
-});
-
-router.get('/patrick', function (req, res) {
-  res.send('I am not a special human being');
-});
-
-router.get('/Mattjarowicz124', function (req, res) {
-  res.send('<img src="https://1.cdn.edl.io/PFEZa720lyk061N3jV0Aoxi3Riwnf1xDQb2gitXOXMO7n9n8.jpg"/>');
-});
-
-router.get('/Jaden', function (req, res) {
-  res.send('IF you get a bigger bed you have bed room but less bedroom');
-});
-
-router.get('/Kevin', function (req, res) {
-  res.send('<img src="https://1.cdn.edl.io/NxweujMTQoV6du3OYqMzyyGjXLNY7QDOogCXDxRqmpjamqwk.jpg"> </i>');
-});
-
-router.get('/dan', function (req, res) {
-  res.send('<span style="font-size: 24pt; color: pink;">lit</span>');
 });
