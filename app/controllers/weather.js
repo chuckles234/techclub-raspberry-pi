a3208bd76c69aa0675e2d738ddb17848123efa63
Luca Santarella <luca.santarella@gmail.com>
2017-10-19 14:25:46 -0400

    Weather api template

--- app/controllers/weather.js
+++ app/controllers/weather.js
@@ -1,9 +1,9 @@
 const express = require('express'),
   request = require('request'),
-  router = express.Router();
+  router = express.Router(),
+  WeatherAPI = require('node-openweathermap');
 
-const WeatherBaseUrl = 'http://api.openweathermap.org/data/2.5/';
-const API_KEY = '602d10eaa16056b9fe71e9d6693e44db';
+const apiKey = '602d10eaa16056b9fe71e9d6693e44db';
 
 module.exports = function (app) {
   app.use('/weather', router);
